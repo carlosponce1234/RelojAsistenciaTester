cd542b806bd091e845318bef05d09bd1293f41c8
Carlos Perez <caos.qwer@gmail.com>
2026-04-13 16:59:38 -0600

    Agregar archivos de proyecto.

---
+++ DeviceManager/ClockDevice.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using zkteco_attendance_api;
+
+namespace RelojAsistenciaTester.DeviceManager
+{
+    internal class ClockDevice
+    {
+        public string Name { get; set; }
+        public string Ubicacion { get; set; }
+        public string IpAdress { get; set; }
+        public int Port { get; set; }
+        public bool UseTcp { get; set; }
+
+        private ZkTeco _zkTeco;
+
+        public ClockDevice(string name, string ubicacion, string ipAdress, int port, bool useTcp)
+        {
+            Name = name;
+            Ubicacion = ubicacion;
+            IpAdress = ipAdress;
+            Port = port;
+            UseTcp = useTcp;
+            _zkTeco = new ZkTeco(IpAdress);
+        }
+
+
+        public void DisplayInfo()
+        {
+            Console.WriteLine($"Name: {Name}");
+            Console.WriteLine($"Ubicacion: {Ubicacion}");
+            Console.WriteLine($"IP Address: {IpAdress}");
+            Console.WriteLine($"Port: {Port}");
+            Console.WriteLine($"Use TCP: {UseTcp}");
+        }
+
+        public void ConectDivice()
+        {
+            var clock = _zkTeco;
+            if (clock.Connect())
+            {
+                Console.WriteLine("Device connected successfully.");
+            }
+            else
+            {
+                Console.WriteLine("Failed to connect to the device.");
+            }
+        }
+
+        public void DisconnectDevice()
+        {
+            _zkTeco.Disconnect();
+        }
+
+        public void GetDeviceInfo()
+        {
+            var clock = _zkTeco;
+            if (clock.IsConnected)
+            {
+                Console.WriteLine("Device connected successfully.");
+                Console.WriteLine($"Device Name: {clock.GetDeviceName}");
+                Console.WriteLine($"Firmware Version: {clock.GetFirmwareVersion}");
+                Console.WriteLine($"Serial Number: {clock.GetDeviceSerial}");
+                clock.Disconnect();
+            }
+            else
+            {
+                Console.WriteLine("Failed to connect to the device.");
+            }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using RelojAsistenciaTester.DeviceManager;
+
+namespace RelojAsistenciaTester
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("==========================================");
+            Console.WriteLine("Reloj de Asistencia Tester");
+            Console.WriteLine("==========================================");
+
+            Console.WriteLine("Ingrese el nombre del dispositivo:");
+            Console.Write("> ");
+            var name = Console.ReadLine();
+            Console.WriteLine("Ingrese la ubicación del dispositivo:");
+            var ubicacion = Console.ReadLine();
+            Console.WriteLine("Ingrese la dirección IP del dispositivo:");
+            var ipAdress = Console.ReadLine();
+            Console.WriteLine("Ingrese el puerto del dispositivo:");
+            var portInput = Console.ReadLine();
+            int port = int.TryParse(portInput, out int parsedPort) ? parsedPort : 0;
+            Console.WriteLine("¿Usar TCP? (s/n):");
+            var useTcpInput = Console.ReadLine();
+            bool useTcp = useTcpInput?.ToLower() == "s";
+            var clockDevice = NewDevice(name, ubicacion, ipAdress, port, useTcp);
+            clockDevice.DisplayInfo();
+            clockDevice.ConectDivice();
+            Console.WriteLine("Presione cualquier tecla para desconectar el dispositivo...");
+            Console.ReadKey();
+            clockDevice.DisconnectDevice();
+
+        }
+
+
+        static ClockDevice NewDevice(string name, string ubicacion, string ipAdress, int port, bool useTcp)
+        {
+            return new ClockDevice(name, ubicacion, ipAdress, port, useTcp);
+        }
+
+
+    }
+}
